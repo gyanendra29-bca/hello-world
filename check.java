f61c0535bbe914f0f6bc6bfb20aa179d12ab88b9
gyanendra tiwari <gyanendratiwari200@gmail.com>
2022-11-14 10:15:27 +0530

    fourth commit

---
+++ check.java
@@ -0,0 +1,29 @@
+import java.util.*;//question 27
+
+import java.util.*;
+class check
+{
+    public static void main(String args[])
+    {
+        double i;
+        System.out.println("enter the value i");
+        Scanner sc=new Scanner(System.in);
+        i=sc.nextDouble();
+        if(i>0)
+        
+          System.out.println("number is postive");
+        
+        else if (i<0)
+        
+        System.out.println(" number is negtive");
+
+        else if (i==0)
+
+        System.out.println("number is zero");
+    
+     else 
+    
+        System.out.println("invalid input");
+    }
+        
+    }
