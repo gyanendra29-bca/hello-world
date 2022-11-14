2d521aa40cc8d5ef098789abd32dc79aa49570dd
gyanendra tiwari <gyanendratiwari200@gmail.com>
2022-11-14 09:21:13 +0530

    ignore

---
+++ adult.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class adult
+{
+   public static void main(String args[])
+   {
+      int n;
+      String s;
+      System.out.println("enter the value of n");
+      Scanner sc=new Scanner(System.in);
+      n=sc.nextInt();
+      s=(n>=18) ? "your are adult": "your are minor";
+      
+   }
+}
+    
