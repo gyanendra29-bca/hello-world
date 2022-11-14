fb049507b7c726e4daf4c5c54f5558adba683f46
gyanendra tiwari <gyanendratiwari200@gmail.com>
2022-11-14 09:58:49 +0530

    git igore add

---
+++ pri.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class pri 
+    {
+        public static void main(String args[])
+        {
+            int p;
+            float r;
+            int t;
+            float hint;
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter inputs : ");
+            p=sc.nextInt();
+            r=sc.nextFloat();
+            t=sc.nextInt();
+            hint=p*r*t/100;
+            System.out.println("simple intrest= "+hint);
+
+        }
+    }
+    
+
